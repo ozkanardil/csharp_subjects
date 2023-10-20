bb71a1b41ded06eaac74c61a7cb071fe32cf7979
Özkan Ardil <ozkanardil@gmail.com>
2023-10-20 22:24:24 +0300

    StackTrace sample project has been created

---
+++ src/StackTrace/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is stack trace?");
+
+FirstMethod();
+
+static void FirstMethod()
+{
+    Console.WriteLine("In first method");
+    SecondMethod();
+}
+
+static void SecondMethod()
+{
+    Console.WriteLine("In second method");
+    ThirdMethod();
+}
+
+static void ThirdMethod()
+{
+    Console.WriteLine("In third method");
+    Console.WriteLine(Environment.StackTrace);
+    Console.WriteLine();
+}
