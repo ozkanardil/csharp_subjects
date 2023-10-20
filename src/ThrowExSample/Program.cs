bf0993ab8cb2ae9be473368cdee6af17f22efe15
Özkan Ardil <ozkanardil@gmail.com>
2023-10-20 22:24:54 +0300

    Throw Ex sample project has been created

---
+++ src/ThrowExSample/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Throw Ex Sample");
+
+try
+{
+    MethodThrowEx();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Throw Ex");
+    Console.WriteLine("Exception caught, logging some information. Stack trace; \n");
+    Console.WriteLine(ex.StackTrace);
+    Console.WriteLine();
+
+    throw;
+}
+
+void MethodThrowEx()
+{
+    try
+    {
+        var collection = Enumerable.Empty<int>();
+        var first = collection.First();
+    }
+    catch (Exception ex)
+    {
+        throw ex;
+    }
+}
