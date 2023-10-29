ec016bfd9cebdbeb7d271ea8394cf6dda58422f0
Özkan Ardil <ozkanardil@gmail.com>
2023-10-29 12:30:33 +0300

    Anonymous types pets project has been created

---
+++ src/AnonymousPets/Pet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousPets
+{
+    internal class Pet
+    {
+        public string Name { get; set; }
+        public PetTypes Type { get; set; }
+        public int Age { get; set; }
+
+        public Pet(string name, PetTypes type, int age)
+        {
+            Name = name;
+            Type = type;
+            Age = age;
+        }
+    }
+}
---
+++ src/AnonymousPets/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AnonymousPets;
+
+Console.WriteLine("Anonymous Pets");
+
+var pets = new[]
+{
+    new Pet("Hannibal", PetTypes.Dog, 12),
+    new Pet("Black", PetTypes.Cat, 8),
+    new Pet("White", PetTypes.Dog, 4),
+    new Pet("Red", PetTypes.Dog, 7),
+    new Pet("Pink", PetTypes.Dog, 7),
+    new Pet("Brown", PetTypes.Dog, 7)
+};
+
+// SIMPLE CODE
+//var averageWeightsData = pets
+//    .GroupBy(pet => pet.Type)
+//    .Select(grouping => grouping.Average(pet => pet.Age));
+
+//foreach (var item in averageWeightsData)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+//MODIFIED CODE USİNG ANONYMOUS TYPE
+var averageWeightsData = pets
+    .GroupBy(pet => pet.Type)
+    .Select(grouping => new
+    {
+        Type = grouping.Key,
+        AgeAverage = grouping.Average(pet=>pet.Age)
+    })
+    .OrderBy(data => data.AgeAverage);
+
+
+foreach (var item in averageWeightsData)
+{
+    Console.WriteLine($"Average age for type {item.Type} is {item.AgeAverage}");
+}
+
+
+Console.ReadLine();
