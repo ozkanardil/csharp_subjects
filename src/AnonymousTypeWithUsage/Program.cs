6cab656c1e6ec203cde7067d2ba3013695308cb7
Özkan Ardil <ozkanardil@gmail.com>
2023-10-29 12:31:03 +0300

    Anonymous types with usage project has been created

---
+++ src/AnonymousTypeWithUsage/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Anonymous Type With Usage \n");
+
+var someData = new { number = 5, text = "My name is Ozkan'" };
+var changedData = someData with { number = 10 };
+
+Console.WriteLine($"SomeData values: number = {someData.number}, text = {someData.text}) \n");
+Console.WriteLine($"ChangedData values: number = {changedData.number}, text = {changedData.text})");
+
+Console.ReadLine();
