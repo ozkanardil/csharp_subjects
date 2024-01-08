4f41e7b4201bf60ac07f9ec62cd930b3249aeab5
Özkan Ardil <ozkanardil@gmail.com>
2024-01-08 16:22:34 +0300

    Reference type when copied sample project has been created

---
+++ src/ReferenceTypeWhenCopy/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Reference type when copied \n");
+
+List<int> listA = new List<int> { 1, 2, 3 };
+List<int> listB = listA;
+
+listB.Add(4);
+
+Console.WriteLine($"ListA contains {listA.Count} elements which are {string.Join(", ", listA)} ");
+Console.WriteLine($"ListB contains {listB.Count} elements which are {string.Join(", ", listB)} ");
