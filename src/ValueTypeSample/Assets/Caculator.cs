0363aee834d7d352878fc57d5ed7262189255d82
Özkan Ardil <ozkanardil@gmail.com>
2024-01-08 16:22:49 +0300

    Value type as parameter sample project has been created

---
+++ src/ValueTypeSample/Assets/Caculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueTypeSample.Assets
+{
+    internal static class Caculator
+    {
+        public static void AddOne(int number)
+        {
+            ++number;
+        }
+    }
+}
---
+++ src/ValueTypeSample/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+using ValueTypeSample.Assets;
+
+Console.WriteLine("Value type as parameter. \n");
+
+int a = 10;
+Console.WriteLine($"a variable is {a}  \n");
+
+Caculator.AddOne(a);
+Console.WriteLine($"Now, new a variable is {a}  \n");
+
+Console.ReadLine();
