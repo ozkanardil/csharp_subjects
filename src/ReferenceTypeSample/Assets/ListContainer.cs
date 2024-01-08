0cd1aca66755b8990e03c320497a3931b7e32fef
Özkan Ardil <ozkanardil@gmail.com>
2024-01-08 16:22:16 +0300

    Reference type as parameter sample project has been created

---
+++ src/ReferenceTypeSample/Assets/ListContainer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceTypeSample.Assets
+{
+    internal static class ListContainer
+    {
+        public static void AddOneToList(List<int> list)
+        {
+            list.Add(1);
+        }
+    }
+}
---
+++ src/ReferenceTypeSample/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ReferenceTypeSample.Assets;
+
+Console.WriteLine("Reference type sample! \n");
+
+var list = new List<int>();    
+Console.WriteLine($"List contains {list.Count} elements \n");
+
+ListContainer.AddOneToList(list);
+Console.WriteLine($"Now, list contains {list.Count} elements \n");
+
+Console.ReadLine();
