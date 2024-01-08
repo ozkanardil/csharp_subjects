8f9717628c45ad31a0b7dcb33f61ee9abfe5a140
Özkan Ardil <ozkanardil@gmail.com>
2024-01-08 16:23:06 +0300

    Value type when copied sample project has been created

---
+++ src/ValueTypeWhenCopySample/Assets/CopyValueType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueTypeWhenCopySample.Assets
+{
+    internal static class CopyValueType
+    {
+        public static void CopyValueTypeMethod(int number)
+        {
+            int b = number;
+            int c = b;
+            ++c;
+
+            Console.WriteLine($"Variable b is {b} \n");
+            Console.WriteLine($"Variable c is {c} \n");
+        }
+    }
+}
---
+++ src/ValueTypeWhenCopySample/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ValueTypeWhenCopySample.Assets;
+
+Console.WriteLine("Value type when copy. \n");
+
+int a = 10;
+Console.WriteLine($"a variable is {a}  \n");
+
+CopyValueType.CopyValueTypeMethod(a);
+Console.WriteLine($"Now, new a variable is {a}  \n");
+
+Console.ReadLine();
