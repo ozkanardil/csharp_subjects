d325dd402acd7fe3e36d8f013226e9db06154f51
Ozkan <ozkan.a@aifalabs.com>
2024-05-24 22:24:36 +0200

    ReadAppsettingsUsingModel project has been added

---
+++ src/AppsettingsReadWithModel/Controllers/ReadAppsettingUsingModelController.cs
@@ -0,0 +1,33 @@
+﻿using AppsettingsReadWithModel.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppsettingsReadWithModel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReadAppsettingUsingModelController : ControllerBase
+    {
+
+        [HttpGet("ReadConnString")]
+        public IActionResult GetConnectionString()
+        {
+            var connString = ApplicationSettings.DbConnString;
+            return Ok(connString);
+        }
+
+        [HttpGet("ReadAppName")]
+        public IActionResult GetAppName()
+        {
+            var connString = ApplicationSettings.SubSettings.Name;
+            return Ok(connString);
+        }
+
+        [HttpGet("ReadGroup")]
+        public IActionResult GetGroup()
+        {
+            var subSettings = ApplicationSettings.SubSettings;
+            return Ok(subSettings);
+        }
+    }
+}
---
+++ src/AppsettingsReadWithModel/Models/ApplicationSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AppsettingsReadWithModel.Models
+{
+    public class ApplicationSettings
+    {
+       
+        public static string DbConnString { get; set; }
+        public static AppsettingsSubsettingsModel SubSettings { get; set; } = new AppsettingsSubsettingsModel();
+    }
+
+    public class AppsettingsSubsettingsModel
+    {
+        public string Name { get; set; }
+        public string ApiVersion { get; set; }
+    }
+
+}
