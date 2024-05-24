fa3a6a496818c965f8395f31f768c07692725f83
Ozkan <ozkan.a@aifalabs.com>
2024-05-24 22:19:26 +0200

    AppsettingsConfigReading project has been added

---
+++ src/AppsettingsConfigReading/Controllers/ReadAppSettingsController.cs
@@ -0,0 +1,42 @@
+﻿using AppsettingsConfigReading.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace AppsettingsConfigReading.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReadAppSettingsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ReadAppSettingsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("ReadConnectionString")]
+        public IActionResult GetConnectionString()
+        {
+            var connectionString = _configuration.GetValue<string>("ApplicationSettings:DbConnString");
+            return Ok(connectionString);
+        }
+
+        [HttpGet("ReadApplicationName")]
+        public IActionResult GetApplicationName()
+        {
+            var name = _configuration.GetValue<string>("ApplicationSettings:SubSettings:Name");
+            return Ok(name);
+        }
+
+        [HttpGet("ReadApplicationGroupData")]
+        public IActionResult ApplicationGroupData()
+        {
+            var appSettings = _configuration.GetSection("ApplicationSettings").Get<AppsettingsModel>();
+            if (appSettings == null) return Ok(null);
+            return Ok(appSettings);
+        }
+
+    }
+}
---
+++ src/AppsettingsConfigReading/Models/AppsettingsModel.cs
@@ -0,0 +1,14 @@
+﻿namespace AppsettingsConfigReading.Models
+{
+    public class AppsettingsModel
+    {
+        public string DbConnString { get; set; }
+        public AppsettingsSubsettingsModel? SubSettings { get; set; }
+    }
+
+    public class AppsettingsSubsettingsModel
+    {
+        public string Name { get; set; }
+        public string ApiVersion { get; set; }
+    }
+}
