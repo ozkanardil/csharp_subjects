fb886e01b785618014ef27b86fc7b876a2816b70
Ozkan <ozkan.a@aifalabs.com>
2024-05-24 22:25:45 +0200

    ReadAppsettingsUsingOptions project has been added

---
+++ src/AppsettingsReadWithOptions/Controllers/ReadAppsettingsUsingOptionsController.cs
@@ -0,0 +1,36 @@
+﻿using AppsettingsReadWithOptions.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace AppsettingsReadWithOptions.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReadAppsettingsUsingOptionsController : ControllerBase
+    {
+        private readonly AppsettingsOptionsModel _appSettingsModel;
+        public ReadAppsettingsUsingOptionsController(IOptions<AppsettingsOptionsModel> options)
+        {
+            _appSettingsModel = options.Value;
+        }
+
+        [HttpGet("ReadConnString")]
+        public IActionResult GetConnectionString()
+        {
+            return Ok(_appSettingsModel.DbConnString);
+        }
+
+        [HttpGet("ReadAppName")]
+        public IActionResult GetAppName()
+        {
+            return Ok(_appSettingsModel.SubSettings.Name);
+        }
+
+        [HttpGet("ReadGroupDate")]
+        public IActionResult GetGroupData()
+        {
+            return Ok(_appSettingsModel);
+        }
+    }
+}
---
+++ src/AppsettingsReadWithOptions/Models/AppsettingsOptionsModel.cs
@@ -0,0 +1,14 @@
+﻿namespace AppsettingsReadWithOptions.Models
+{
+    public class AppsettingsOptionsModel
+    {
+        public string DbConnString { get; set; }
+        public AppsettingsSubsettingsModel? SubSettings { get; set; }
+    }
+
+    public class AppsettingsSubsettingsModel
+    {
+        public string Name { get; set; }
+        public string ApiVersion { get; set; }
+    }
+}
